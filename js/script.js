df68a1bb3dc01ff1089a4255aff4eb5f85ac972f
Hamilton Cesar <hamiltondc.dc@gmail.com>
2022-08-10 08:12:03 -0300

    finalizado

--- js/script.js
+++ js/script.js
@@ -27,8 +27,8 @@ function calcular() {
     let qdtTotalRefrigerante = RefrigerantePP(duracao) * adultos + (RefrigerantePP(duracao) / 2 * criancas);
     let qdtTotalPao = PaoPP(duracao) * adultos + (PaoPP(duracao) / 2 * criancas);
 
-
-    resultado.innerHTML = `<p>${qdtTotalCarne / 1000} Kg de Carne <p>`
+    resultado.innerHTML = '<h2 class="result-final">Você vai precisar de :</h2>'
+    resultado.innerHTML += `<p>${qdtTotalCarne / 1000} Kg de Carne <p>`
     resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja / 355)} latas de Cerveja <p>`
     resultado.innerHTML += `<p>${Math.ceil(qdtTotalRefrigerante / 2000)} litros de refrigerante <p>`
     resultado.innerHTML += `<p>${Math.ceil(qdtTotalPao)} Pacotes de Pão de alho`
